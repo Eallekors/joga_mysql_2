6b9651eb375358aa6f010f8932f65a38a1e635d1
Eallekors <erkxalle@gmail.com>
2024-09-06 13:14:56 +0300

    Displayed author article list using the author and article oop model

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ app.use(bodyParser.json());
 
 const articleControllerClass = require('./controllers/article')
 const articleController = new articleControllerClass()
+const authorControllerClass = require('./controllers/author')
+const authorController = new authorControllerClass();
+
 
 const articleRoutes = require('./routes/article');
 app.get('/', (req, res) => articleController.getAllArticles(req, res));
 app.get('/article/:slug', (req, res) => articleController.getArticleBySlug(req, res));
+app.get('/author/:id', (req, res) => authorController.getAuthorById(req, res));
+
 
 app.listen(3025, () => {
     console.log('App is started at http://localhost:3025')

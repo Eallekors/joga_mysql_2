914af807bac440231f825dcb89e25e5a1e4bf267
Eallekors <erkxalle@gmail.com>
2024-09-10 12:21:43 +0300

    Fix existing user and password limitation errors

--- controllers/user.js
+++ controllers/user.js
@@ -1,27 +1,42 @@
+const pool = require('../utils/db'); // Replace with your MySQL pool configuration
 const bcrypt = require('bcrypt')
 const userDbModel = require('../models/user')
 const UserModel = new userDbModel()
 
+
 class userController {
     async register(req, res){
-        bcrypt.hash(req.body.password, 10, (error, cryptPassword) => {
-            UserModel.create({
-                username: req.body.username,
-                email: req.body.email,
-                password: cryptPassword
-            })
-            .then((registered) => {
-                req.session.user = {
-                    username: registered.username,
-                    user_id: registered.id
+                console.log(req.body.password.length)
+                if(req.body.password.length < 10){
+                    return res.status(400).json({ message: 'Password is too short' });
+            
+                }else{
+                const cryptPassword = await bcrypt.hash(req.body.password, 10);    
+                const newUser = 
+                    {
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: cryptPassword
+                    }
+                const check = await UserModel.findOne(req.body.username);
+                
+                if(check){
+                        return res.status(400).json({ message: 'Username already exists' });
+                }else{
+                    UserModel.create(newUser)
+                    .then((registered) => {
+                        req.session.user = {
+                            username: registered.username,
+                            user_id: registered.id
+                        }
+                        res.json({
+                            message: 'New user is registered',
+                            user:registered,
+                            user_session: req.session.user
+                        })
+                    })
                 }
-                res.json({
-                    message: 'New user is registered',
-                    user:registered,
-                    user_session: req.session.user
-                })
-            })
-        })
+            }
     }
 }
 
--- models/user.js
+++ models/user.js
@@ -4,6 +4,12 @@ class UserModel extends BaseSQLModel {
     constructor() {
         super('user')
     }
+
+    async findOne(username){
+        const account = await super.findOne('username', username)
+        return account;
+    }
+
     async create(user){
         const createdUserId = await super.create(user);
         return createdUserId;
